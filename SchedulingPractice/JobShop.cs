65123837b3536bf420940df01607450cade1f1f0
610sk <610sky@naver.com>
2026-06-30 16:45:21 +0900

    0630수정

---
+++ SchedulingPractice/JobShop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Google.OrTools.Sat;
+
+namespace SchedulingPractice;
+
+
+
+public class JobShop
+{
+    
+}
--- SchedulingPractice/MainWindow.xaml.cs
+++ SchedulingPractice/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Google.OrTools.Sat;
 
 namespace SchedulingPractice;
 
@@ -19,5 +20,57 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        var allJobs =
+            new[]
+            {
+                new[]
+                    {
+                        // job 0
+                        new { machine = 0, duration = 3 }, // task 0
+                        new { machine = 1, duration = 2 }, // task 1
+                        new { machine = 2, duration = 2 }, // task 2
+                    }
+                    .ToList(),
+                new[]
+                    {
+                        // job 1
+                        new { machine = 0, duration = 2 }, // task 0
+                        new { machine = 2, duration = 1 }, // task 1
+                        new { machine = 1, duration = 4 }, // task 2
+                    }
+                    .ToList(),
+                new[]
+                {
+                    // job 2
+                    new { machine = 1, duration = 4 }, // task 0
+                    new { machine = 2, duration = 3 }, // task 1
+                }.ToList(),
+            }.ToList();
+
+        int numMachines = 0;
+        foreach (var job in allJobs)
+        {
+            foreach (var task in job)
+            {
+                numMachines = Math.Max(numMachines, 1 + task.machine);
+            }
+        }
+
+        int[] allMachines = Enumerable.Range(0, numMachines).ToArray();
+
+        int horizon = 0;
+        foreach (var job in allJobs)
+        {
+            foreach (var task in job)
+            {
+                horizon += task.duration;
+            }
+        }
+        
+        // 모델 선언
+        CpModel model = new CpModel();
+        
+        // Dictionary<Tuple<int, int>, Tuple<IntVar,IntVar,IntervalVar>> allTasks=new Dictionary<Tuple
     }
 }
